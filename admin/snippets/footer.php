d8455cda70cc08d1c2f01abfd07e26b2d78d290b
Kyle Daniel Javines <javineskyle123@gmail.com>
2024-05-24 13:53:05 +0800

    fixes index page, added footer

---
+++ admin/snippets/footer.php
@@ -0,0 +1,12 @@
+<div class="year-section-subject">
+            <p>Course: Bachelor of Science in Information Technology</p>
+            <p>Section: 3A</p>
+            <p>Subject: Applications Development and Emerging Technologies</p>
+        </div>
+        <div class="group-members">
+            <p>Group Members:</p>
+            <p>Alhine Stephen Baliton</p>
+            <p>Marvine Ray Fernandez</p>
+            <p>Kyle Daniel Javines</p>
+            <p>Augustine Lar Reyes</p>
+</div>
